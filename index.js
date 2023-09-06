2ebf6905742de5f693dd1956302be46e33621eb8
景雪峰 <xuefeng.jing@matrixport.com>
2023-09-06 17:23:16 +0800

    test gitflow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {imgInit} from './modules/imgUpload.js'
 import {v4} from './web_modules/uuid.js'
 console.log(v4())
+console.log(v4())
 alert(v4())
 
 // document.addEventListener('paste', imgInit)
